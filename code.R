7f65ec363a16867b97632c081d141b4dc0a70c3c
Alan Lue <alan.lue@gmail.com>
2010-05-17 12:42:10 -0700

    Checkpoint.

--- code.R
+++ code.R
@@ -15,8 +15,7 @@ fde(s, k, r, t, sd, n, m)
 
 ##
 
-optionrc <- list(s=50, k=50, r=0.10, t=5/12, sd=0.40, type='put',
-                 style='american')
+optionrc <- list(s=50, k=50, r=0.10, t=5/12, sd=0.40, type='p', style='a')
 with(optionrc, {
   p.bsm <- bsm.option(s=s, k=k, r=r, t=t, sd=sd, type=type)
   p.fde <- fde(s=s, k=k, r=r, t=t, sd=sd, n=10, m=20, type=type, style=style)
@@ -25,4 +24,15 @@ with(optionrc, {
   print(p.fde)
 })
 with(optionrc, fde(s, k, r, t, sd, n=10, m=20, type=type, style=style))
-rotate(with(optionrc, fde(s,k,r,t,sd,n=10,m=20,type,style,grid=T)), digits=2)
+with(optionrc, rotate(fde(s,k,r,t,sd,n=10,m=20,type,style,grid=T), digits=2))
+
+
+optionrc <- list(s=50, k=50, r=0.10, t=5/12, sd=0.40, type='p', style='a')
+## Binomial
+with(optionrc, binom.american.put(s, k, r, t, sd, n=1e3))
+## Explicit finite difference
+with(optionrc, fde(s, k, r, t, sd, n=10, m=20, type=type, style=style))
+with(optionrc, rotate(fde(s,k,r,t,sd,n=10,m=20,type,style,grid=T), digits=2))
+## Explicit finite difference, log transform
+with(optionrc, fde.log(s, k, r, t, sd, n=10, m=20, type, style))
+with(optionrc, rotate(fde.log(s,k,r,t,sd,n=10,m=20,type,style,grid=T), 2))
--- functions.R
+++ functions.R
@@ -10,14 +10,14 @@ fde <- function(s, k, r, t, sd, n = ceiling(1e3*t), m = 2*ceiling(sqrt(n)),
     function(x, tol = .Machine$double.eps^0.5)  abs(x - round(x)) < tol
   if (!is.wholenumber(n) || n <= 0) stop("n = ",n," is not a positive integer!")
   if (!is.wholenumber(m) || m <= 0) stop("m = ",m," is not a positive integer!")
-  type <- match.arg(type)
-  style <- match.arg(style)
+  type <- match.arg(type); style <- match.arg(style)
 
   dt <- t / n
   m <- m + m%%2                         # Ensure m is even.
+  ## FIXME: s.max depends on k, t, sd
   s.lim <- c(max=2*s, min=0)
   ds <- unname(s.lim['max'] - s.lim['min']) / m
-  s.seq <- s.lim['min'] + 0:m*ds            # vector, m+1 elements
+  s.seq <- s.lim['min'] + 0:m*ds        # vector, m+1 elements
 
   f <- matrix(rep(NA, (n+1)*(m+1)), nrow=n+1)
   g2m <- function(i)  i + 1             # grid index to matrix index
@@ -30,77 +30,77 @@ fde <- function(s, k, r, t, sd, n = ceiling(1e3*t), m = 2*ceiling(sqrt(n)),
       c <- (1 + r*dt)^-1 * (1/2*r*j*dt + 1/2*sd^2*j^2*dt)
       f[i,g2m(j)] <- t(c(a,b,c)) %*% f[i+1,g2m((j-1):(j+1))]
     }
-    if (style == 'american')
-      f[i,g2m((m-1):1)] <-
-        pmax(f[i,g2m((m-1):1)], switch(type, call = s.seq[g2m((m-1):1)] - k,
-                                       put = k - s.seq[g2m((m-1):1)]))
-    f[i,g2m(m)] <- switch(type,         # call: ∂C/∂S ≈ 1; put: ∂C/∂S ≈ 0
-                          call = f[i,g2m(m-1)]+s.seq[g2m(m)]-s.seq[g2m(m-1)],
-                          put = f[i,g2m(m-1)])
-    f[i,g2m(0)] <- switch(type,         # call: ∂C/∂S ≈ 0; put: ∂C/∂S ≈ 1
-                          call = f[i,g2m(1)],
-                          put = f[i,g2m(1)] - (s.seq[g2m(m)] - s.seq[g2m(m-1)]))
-    if (style == 'american') {
-      f[i,g2m(m)] <- pmax(f[i,g2m(m)], switch(type, call = s.seq[g2m(m)] - k,
-                                              put = k - s.seq[g2m(m)]))
-      f[i,g2m(0)] <- pmax(f[i,g2m(0)], switch(type, call = s.seq[g2m(0)] - k,
-                                              put = k - s.seq[g2m(0)]))
+
+    if (type == 'call') {               # m: ∂C/∂S ≈ 1
+      f[i,g2m(m)] <- f[i,g2m(m-1)] + s.seq[g2m(m)] - s.seq[g2m(m-1)]
+      f[i,g2m(0)] <- f[i,g2m(1)]        # 0: ∂C/∂S ≈ 0
+    }
+    else if (type == 'put') {           # m: ∂C/∂S ≈ 0
+      f[i,g2m(m)] <- f[i,g2m(m-1)]      # 0: ∂C/∂S ≈ 1
+      f[i,g2m(0)] <- f[i,g2m(1)] - (s.seq[g2m(1)] - s.seq[g2m(0)])
+      if (style == 'american')
+        f[i,] <- pmax(f[i,], k - s.seq)
     }
   }
 
-  if (grid)
-    return(f)
-  else
-    return(f[g2m(0), g2m(m/2)])
+  if (grid) return(f)
+  else return(f[g2m(0), g2m(m/2)])
 }
 
 fde.log <- function(s, k, r, t, sd, n = ceiling(1e3*t), m = 2*ceiling(sqrt(n)),
-                    type = c("call", "put"), optim = TRUE) {
+                    type = c("call", "put"), style = c("european", "american"),
+                    grid = FALSE, optim = TRUE) {
   if (t <= 0) stop("t = ", t, " is nonpositive!")
   is.wholenumber <-
     function(x, tol = .Machine$double.eps^0.5)  abs(x - round(x)) < tol
   if (!is.wholenumber(n) || n <= 0) stop("n = ",n," is not a positive integer!")
   if (!is.wholenumber(m) || m <= 0) stop("m = ",m," is not a positive integer!")
-  type <- match.arg(type)
+  type <- match.arg(type); style <- match.arg(style)
 
   dt <- t / n
   m <- m + m%%2                         # Ensure m is even.
   ## Set stock price limits to +/- 3 standard deviations.
-  z.lim <- c(max=log(s) + (r-1/2*sd^2)*t + sd*3*sqrt(t),
-             min=log(s) + (r-1/2*sd^2)*t - sd*3*sqrt(t))
+  z.lim <- log(s) + (r-1/2*sd^2)*t + c(max=sd*3*sqrt(t), min=-sd*3*sqrt(t))
   dz <- unname(z.lim['max'] - z.lim['min']) / m
-  z <- z.lim['min'] + 0:m*dz            # vector, m+1 elements
+  z.seq <- z.lim['min'] + 0:m*dz        # vector, m+1 elements
 
   f <- matrix(rep(NA, (n+1)*(m+1)), nrow=n+1)
   g2m <- function(i)  i + 1             # grid index to matrix index
-  f[g2m(n),] = switch(type, call=pmax(exp(z) - k, 0), put=pmax(k - exp(z), 0))
+  f[g2m(n),] = switch(type, call=pmax(exp(z.seq)-k,0), put=pmax(k-exp(z.seq),0))
 
   a <- (1 + r*dt)^-1 * (-dt/(2*dz)*(r - 1/2*sd^2) + dt/(2*dz^2)*sd^2)
   b <- (1 + r*dt)^-1 * (1 - dt/dz^2*sd^2)
   c <- (1 + r*dt)^-1 * (dt/(2*dz)*(r - 1/2*sd^2) + dt/(2*dz^2)*sd^2)
   for (i in g2m((n-1):0)) {             # Iterate from end to beginning.
-    if (optim)
-      f[i,g2m(1:(m-1))] <- matrix(f[i+1,rep(g2m(0:2),m-1)+rep(0:(m-2),each=3)],
-                                  ncol=3, byrow=TRUE) %*% c(a,b,c)
+    if (optim) {
+      j.seq <- rep(g2m(0:2), times=m-1) + rep(0:(m-2), each=3)
+      f[i,g2m(1:(m-1))] <- matrix(f[i+1,j.seq], ncol=3, byrow=TRUE) %*% c(a,b,c)
+    }
     else
       for (j in g2m((m-1):1))
         f[i,j] <- t(c(a,b,c)) %*% f[i+1,(j-1):(j+1)]
-    f[i,g2m(m)] <- switch(type,         # call: ∂C/∂S ≈ 1; put: ∂C/∂S ≈ 0
-                          call = f[i,g2m(m-1)]+exp(z[g2m(m)])-exp(z[g2m(m-1)]),
-                          put = f[i,g2m(m-1)])
-    f[i,g2m(0)] <- switch(type,         # call: ∂C/∂S ≈ 0; put: ∂C/∂S ≈ 1
-                          call = f[i,g2m(1)],
-                          put = f[i,g2m(1)] - (exp(z[g2m(m)])-exp(z[g2m(m-1)])))
+
+    if (type == 'call') {               # m: ∂C/∂S ≈ 1
+      f[i,g2m(m)] <- f[i,g2m(m-1)] + exp(z.seq[g2m(m)]) - exp(z.seq[g2m(m-1)])
+      f[i,g2m(0)] <- f[i,g2m(1)]        # 0: ∂C/∂S ≈ 0
+    }
+    else if (type == 'put') {           # m: ∂C/∂S ≈ 0
+      f[i,g2m(m)] <- f[i,g2m(m-1)]      # 0: ∂C/∂S ≈ 1
+      f[i,g2m(0)] <- f[i,g2m(1)] - (exp(z.seq[g2m(m)]) - exp(z.seq[g2m(m-1)]))
+      if (style == 'american')
+        f[i,] <- pmax(f[i,], k - exp(z.seq))
+    }
   }
 
-  f[g2m(0), g2m(m/2)]
+  if (grid) return(f)
+  else return(f[g2m(0), g2m(m/2)])
 }
 
 ######################################################################
 ## Black-Scholes-Mertons Option Pricing                             ##
 ######################################################################
 
-bsm.d1 <- function (s, k, r, t, sd) {
+bsm.d1 <- function(s, k, r, t, sd) {
   (log(s/k) + (r + 1/2*sd^2)*t) / (sd*sqrt(t))
 }
 
@@ -108,7 +108,7 @@ bsm.d2 <- function(s, k, r, t, sd) {
   bsm.d1(s, k, r, t, sd) - sd*sqrt(t)
 }
 
-bsm.option <- function (s, k, r, t, sd, type = c("call", "put")) {
+bsm.option <- function(s, k, r, t, sd, type = c("call", "put")) {
   type <- match.arg(type)
   d1 <- bsm.d1(s, k, r, t, sd)
   d2 <- bsm.d2(s, k, r, t, sd)
@@ -116,4 +116,28 @@ bsm.option <- function (s, k, r, t, sd, type = c("call", "put")) {
               call = s*pnorm(d1) - k*exp(-r*t)*pnorm(d2),
               put = k*exp(-r*t)*pnorm(d2,lower.tail=F)-s*pnorm(d1,lower.tail=F))
   return(r)
+}
+
+######################################################################
+## Binomial Option Pricing                                          ##
+######################################################################
+
+binom.american.put <- function(s, k, r, t, sd, n,
+                               u = exp(sd*sqrt(t/n)), d = 1/u,
+                               p = (exp(r*t/n) - d) / (u - d)) {
+  n.u <- n:0
+  s.t <- s * u^n.u * d^(n - n.u)
+  v <- pmax(k - s.t, 0)
+  for (i in (n-1):0) {
+    w <- matrix(rep(v, c(1, rep(2, times=length(v)-2), 1)), ncol=2, byrow=TRUE)
+    cv <- exp(-r*t/n) * (p*w[,1] + (1-p)*w[,2])
+
+    n.u <- i:0
+    s.i <- s * u^n.u * d^(i - n.u)
+    ev <- pmax(k - s.i, 0)
+
+    v <- pmax(cv, ev)
+  }
+  
+  return(v)
 }
