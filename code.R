35caa98b2ad0c9faab76a2d85c8b43223d5aa008
Alan Lue <alan.lue@gmail.com>
2010-05-17 10:19:04 -0700

    Improved execution code.

--- code.R
+++ code.R
@@ -14,11 +14,11 @@ fde(s, k, r, t, sd, n, m)
 
 ##
 
-s <- 50
-k <- 50
-r <- 0.10
-sd <- 0.40
-t <- 5/12
-
-bsm.option(s, k, r, t, sd, type='call')
-fde.s(s, k, r, t, sd, n=10, m=20, type='put', style='american')
+optionrc <- list(s=50, k=50, r=0.10, t=5/12, sd=0.40, type='put',
+                 style='american')
+with(optionrc, {
+  p.bsm <- bsm.option(s=s, k=k, r=r, t=t, sd=sd, type=type)
+  p.fde <- fde(s=s, k=k, r=r, t=t, sd=sd, n=10, m=20, type=type, style=style)
+  cat('bsm = ', p.bsm, '\n', sep="")
+  cat('fde = ', p.fde, '\n', sep="")
+})
